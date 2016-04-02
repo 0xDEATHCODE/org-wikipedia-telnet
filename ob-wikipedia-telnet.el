df1100e3900d48464bf3e08f351292047e0ed7dc
stardiviner <numbchild@gmail.com>
2016-04-02 14:36:18 +0800

    add library headers.

---
+++ ob-wikipedia-telnet.el
@@ -0,0 +1,39 @@
+;;; ob-wikipedia-telnet.el --- Add Wikipedia telnet into Org-mode Babel support.
+;;; -*- coding: utf-8 -*-
+
+;; Copyright (C) 2016 Free Software Foundation, Inc.
+
+;; Authors: Tristan <tristan.j.huang@gmail.com>
+;;   stardiviner <numbchild@gmail.com>
+;; Keywords: wikipedia, telnet, babel, link
+;; homepage: https://github.com/0xDEATHCODE/org-wikipedia-telnet
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Requirements:
+
+;;; Code:
+
+
+
+
+
+
+(provide 'ob-wikipedia-telnet)
+
+;;; ob-wikipedia-telnet.el ends here
